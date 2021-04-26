2347d015f5c9304aa2cc04576872662357834dd7
Enni Dahlström <n0daen00@students.oamk.fi>
2021-04-26 11:06:46 +0300

    editProduct.php

--- products/editProduct.php
+++ products/editProduct.php
@@ -2,13 +2,38 @@
 require_once '../inc/functions.php';
 require_once '../inc/headers.php';
 
-$tuotenro=filter_input(INPUT_POST,'tuotenro',FILTER_SANITIZE_NUMBER_INT);
+$uri = parse_url(filter_input(INPUT_SERVER,'PATH_INFO'),PHP_URL_PATH);
+$parameters = explode('/',$uri);
+$tuotenro = $parameters[1];
+
 $tuotenimi=filter_input(INPUT_POST,'tuotenimi',FILTER_SANITIZE_STRING);
 $hinta=filter_input(INPUT_POST,'hinta',FILTER_SANITIZE_STRING);
 $kuvaus=filter_input(INPUT_POST,'kuvaus',FILTER_SANITIZE_STRING);
 $trnro=filter_input(INPUT_POST,'trnro',FILTER_SANITIZE_NUMBER_INT);
 
-try {
+if (isset($_FILES['file'])) {
+    if ($_FILES['file']['error'] === UPLOAD_ERR_OK) {
+      $filename = $_FILES['file']['name'];
+      $type = $_FILES['file']['type'];
+  
+      if ($type === 'image/png')  {
+        $path = '../img/' . basename($filename);
+        if (move_uploaded_file($_FILES['file']['tmp_name'],$path)) {
+            $db = openDb();
+            $db->beginTransaction();
+            $kysely = $db->prepare("UPDATE tuote SET tuotenimi='$tuotenimi', hinta='$hinta', kuvaus='$kuvaus', trnro = '$trnro', image = '$filename' WHERE tuotenro = $tuotenro");
+            $kysely->execute();
+            $db->commit();
+        } else {
+            returnError("Tiedoston tallentaminen img-kansioon epäonnistui");
+        }
+    } else {
+        returnError("Kuvan tyyppi virheellinen");
+    }
+} else {
+    returnError("Kuvaa ei voitu ladata selaimelta");
+}
+} else {
     $db = openDb();
     
     $kysely = $db->prepare("UPDATE tuote SET tuotenimi = :tuotenimi, hinta = :hinta, kuvaus = :kuvaus, trnro = :trnro WHERE tuotenro = :tuotenro");
@@ -19,11 +44,4 @@ try {
     $kysely->bindValue(':kuvaus',$kuvaus,PDO::PARAM_STR);
     $kysely->bindValue(':trnro',$trnro,PDO::PARAM_INT);
     $kysely->execute();
-    
-
-    header("Location: http://localhost:3000/MuokkaaTuotteita");
-    exit();
-}
-catch (PDOException $pdoex) {
-    returnError($pdoex);
 }
