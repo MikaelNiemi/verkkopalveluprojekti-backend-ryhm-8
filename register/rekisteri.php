7390c1801b37f79dba2de04998f7ffac732a7b6c
niko <n0rani00@students.oamk.fi>
2021-04-29 10:37:42 +0300

    rekisterin hashin muuttaminen

--- register/rekisteri.php
+++ register/rekisteri.php
@@ -8,29 +8,16 @@
   $salasana = filter_input(INPUT_POST,"salasana", FILTER_SANITIZE_STRING);
   $postinro = filter_input(INPUT_POST, "postinro", FILTER_SANITIZE_NUMBER_INT);
   $lahiosoite = filter_input(INPUT_POST, "lahiosoite", FILTER_SANITIZE_STRING);
-   $salasanahash=hash("sha256", $salasana);
-   $salasana = $salasanahash;
-   
-  // $salasana2 = filter_input(INPUT_POST, "salasana2", FILTER_SANITIZE_STRING);
+  $salasanahash=hash("sha256", $salasana);
+  //$salasana = $salasanahash;
+  //$salasana2 = filter_input(INPUT_POST, "salasana2", FILTER_SANITIZE_STRING);
+ 
+      try {
+      
+        $db = openDb();
+        selectAsJson($db," INSERT INTO asiakas (sukunimi,etunimi,email,salasana,postinro,lahiosoite) VALUES ('$sukunimi','$etunimi','$email','$salasanahash','$postinro','$lahiosoite')");
 
-try {
-  $db = openDb();
-  selectAsJson($db," INSERT INTO asiakas (sukunimi,etunimi,email,salasana,postinro,lahiosoite) VALUES ('$sukunimi','$etunimi','$email','$salasana','$postinro','$lahiosoite')");
-
-  print "<p>onnistu</p>";
-
-} catch (PDOException $pdoex) {
-    returnError($pdoex);
+      } catch (PDOException $pdoex) {
+          returnError($pdoex);
 }
-
-//  create table asiakas (
-// 	asnro int primary key auto_increment,
-// 	sukunimi varchar(30) not null,
-// 	etunimi varchar(30) not null,
-// 	email varchar(30) not null,
-// 	salasana varchar(30) not null,
-// 	lahiosoite varchar(30),
-// 	postinro char(5),
-// 	astili_luotu TIMESTAMP DEFAULT CURRENT_TIMESTAMP not null,
-// 	constraint asnro_uniikki unique (asnro),
-// 	constraint email_uniikki unique (email)); 
+      
